99b45e26bfc216d973743b09d68c343882a15953
Devon Burnham <novedevo@gmail.com>
2019-09-30 20:38:54 -0700

    added/fixed user input, minor refactor

--- nibbler.cpp
+++ nibbler.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
+
 using namespace std;
 
 int nibbler(unsigned long int a, unsigned long int b)
 {
     unsigned long int c = 0;
     unsigned long int d = 0;
-    unsigned long int e = 0;
     
     c = (a&0xF)^((a&0xF0)>>4)^((a&0xF00)>>8)^((a&0xF000)>>12)^((a&0xF0000)>>16)^((a&0xF00000)>>20)^((a&0xF000000)>>24)^((a&0xF0000000)>>28)^((a&0xF00000000)>>32);
     d = (b&0xF)^((b&0xF0)>>4)^((b&0xF00)>>8)^((b&0xF000)>>12)^((b&0xF0000)>>16)^((b&0xF00000)>>20)^((b&0xF000000)>>24)^((b&0xF0000000)>>28)^((b&0xF00000000)>>32);
-
-    e = d|(c<<4);
-    return e;
+    return d|(c<<4);
 }
 
 int main(){
-    int test = 0;
-    cout<<"test: ";
-    cin>>test;
-    cout<<nibbler(test, 0x5);
+    unsigned long int a = 0;
+    unsigned long int b = 0;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    cout << nibbler(a, b);
 }
