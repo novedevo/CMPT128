d3bf31561358424577f0d293799d5afa28b4b2ae
Devon Burnham <novedevo@gmail.com>
2019-10-06 15:54:39 -0700

    Codewrite Assignments added and fixed

--- src/nibbler.cpp
+++ src/nibbler.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
-#include <limits.h>
-
+#include <iomanip>
 using namespace std;
+#include<stdio.h>
 
-int nibbler(unsigned long int a, unsigned long int b)
-{
+int nibbler(unsigned long int a, unsigned long int b){
     unsigned long int c = 0;
     unsigned long int d = 0;
     
-    c = (a&0xF)^((a&0xF0)>>4)^((a&0xF00)>>8)^((a&0xF000)>>12)^((a&0xF0000)>>16)^((a&0xF00000)>>20)^((a&0xF000000)>>24)^((a&0xF0000000)>>28)^((a&0xF00000000)>>32);
-    d = (b&0xF)^((b&0xF0)>>4)^((b&0xF00)>>8)^((b&0xF000)>>12)^((b&0xF0000)>>16)^((b&0xF00000)>>20)^((b&0xF000000)>>24)^((b&0xF0000000)>>28)^((b&0xF00000000)>>32);
-    return d|(c<<4);
+    for(unsigned long int i = 0; i<16; i++){
+        c=c^((((unsigned long int)0xF)<<(4*(15-i))&a))>>4*(15-i);
+        d=d^((((unsigned long int)0xF)<<(4*(15-i))&b))>>4*(15-i);
+    }
+    return (d|(c<<4));
 }
 
 int main(){
-    unsigned long int a = 0;
-    unsigned long int b = 0;
-    cout << "a: ";
-    cin >> a;
-    cout << "b: ";
-    cin >> b;
-    cout << nibbler(a, b);
+    printf( "%d", nibbler( 897345879634, 789441719 ));
 }
