f6d32852978b3772edb6f814397ffb94bd0ab3ec
Devon Burnham <novedevo@gmail.com>
2019-10-08 13:55:09 -0700

    modified vector splitter

--- src/system-of-particles.cpp
+++ src/system-of-particles.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 pair<vector<double>, vector<double>> xCoM(const vector<double> & v) {
     
-    if (v.empty()) {
-        cout<<"please enter values";
+    if (v.empty()||v.size()%2!=0) {
+        cout<<"please enter correct set of values";
         exit(EXIT_FAILURE);
     }
 
     vector<double> masses;
     vector<double> positions;
 
-    for (size_t i = 0; i<v.size(); i+=2){
+    for (size_t i = 0; i<v.size(); i++){
         masses.push_back(v[i]);
-        positions.push_back(v[i+1]);
+        positions.push_back(v[i+v.size()/2]);
     }
 
     return make_pair(masses, positions);
@@ -25,6 +25,6 @@ pair<vector<double>, vector<double>> xCoM(const vector<double> & v) {
 
 
 int main(){
-    pair <vector<double>,vector<double>> p = xCoM({8.6, 2.0, 1.3, 2.5});
-    cout<<get<0>(p)[1];
+    pair <vector<double>,vector<double>> p = xCoM({8.6, 9.4, 8.3, 9.1});
+    cout<<get<1>(p)[1];
 }
