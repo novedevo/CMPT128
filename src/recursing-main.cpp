0fca4ee89a376b3647fbd8e1d9d4403979daffab
Devon Burnham <novedevo@gmail.com>
2019-10-10 18:36:53 -0700

    cursed

--- src/recursing-main.cpp
+++ src/recursing-main.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
-using namespace std;
-//int i = 0;
 
-int main(int i){
-    if (i>6) return 0;
-    cout<<i<<" ";
-    main(++i);
-}
+#define I_refuse_to_close_this )
+#define or_this }
+#define no_semicolons ;
+
+using namespace std no_semicolons
+//int i = 0 no_semicolons
+
+int main ( int i I_refuse_to_close_this {
+    if (i>6 I_refuse_to_close_this return 0 no_semicolons
+    cout<<i<<" " no_semicolons
+    main ( ++i I_refuse_to_close_this no_semicolons
+or_this
