69840183618e3bb5d44ca33bbfd94da451d2ccfc
Devon Burnham <novedevo@gmail.com>
2019-11-05 11:10:54 -0800

    Fixed smoothness calculator

--- src/smoothness-calculator.cpp
+++ src/smoothness-calculator.cpp
@@ -4,67 +4,81 @@
 #include <cmath>
 using namespace std;
 
-//TODO: everything is broken pls help
-
-class MyInputFileType : public ifstream{
+//* adds custom member methods, constants, and variables
+class MyInputFileType : public ifstream {
     private:
-
-        const short int MINHEIGHT = -100;
+        const short int MINHEIGHT = -100;   //* minimum & maximum y-values that are acceptable
         const short int MAXHEIGHT = 300;
         int lineNumber = 1;
+        int numMeasured = 0;    //*number of values that have been recorded from the file
         double newHeight = 0.0;
-        int numOfMeasurements = 0;
-        double sum1 = 0.0;
-        double sum2 = 0.0;
+        int numOfMeasurements = 0;  //*stores user-defined number of times data was measured
+        double sum1 = 0.0;  //accumulates y-values
+        double sum2 = 0.0;  //accumulates squared y-values
         double mean = 0.0;
         double rms = 0.0;
-        double maxy = MINHEIGHT;
-        double miny = MAXHEIGHT;
+        double maxy = MINHEIGHT;    //maximum y-value that was read
+        double miny = MAXHEIGHT;    //minumum y-value that was read
         double maxRough = 0.0;
     
     public:
-        char inputName[80] = {'\0'};
+        char inputName[80] = {'\0'};    //strings to store input filename
+        
         void getInput(void){
             cout << "Enter the input filename:\n";
             cin >> inputName;
         }
 
-        void getMeasurements(void){
-            while (!eof() && (*this >> newHeight)){
+        int getMeasurements(void){
+            while (!eof() && (*this >> newHeight)){ //while file continues and data can correctly be read into a float
+                ////cout<<endl<<"line number is "<<lineNumber<<endl;
+                if (lineNumber > numOfMeasurements){break;} //exit loop if we have measured enough data
+                
                 lineNumber++;
-                if (lineNumber > numOfMeasurements){break;}
-                if (newHeight > MINHEIGHT && newHeight < MAXHEIGHT){
-                    cout << setprecision(3) << newHeight << endl;
+                
+                if (newHeight > MINHEIGHT && newHeight < MAXHEIGHT){    //if within bounds
+                    cout << newHeight << endl;
+                    
                     sum1 += newHeight;
-                    cout<<endl<<"Sum1 is "<<sum1<<endl;
                     sum2 += newHeight*newHeight;
-                    cout<<"Sum2 is "<<sum2<<endl<<endl;
+                    
                     if (newHeight>=maxy) maxy = newHeight;
                     if (newHeight<=miny) miny = newHeight;
+                    
+                    numMeasured++;
 
                 }else{
-                    cerr << "ERROR: Surface is too rough. Measurement on line " << lineNumber << " will not be used";
+                    cerr << "ERROR: Surface is too rough. Measurement on line " << lineNumber << " will not be used"<<endl;
                 }
 
             }
 
-            if (eof()&&lineNumber<(numOfMeasurements+1)){
+            if (eof()&&lineNumber<=numOfMeasurements){
                 cerr << "ERROR: Insufficient Data, data ends at line " << lineNumber <<endl;
                 numOfMeasurements = lineNumber-1;
+                return 1;
+            }else if (lineNumber > numOfMeasurements){
+                return 0;
+            
             }else if (!eof()){
                 cerr << "ERROR: Incorrect data in input data file at line " << lineNumber <<endl;
                 numOfMeasurements = lineNumber-1;
+                return 1;
             }
         }
         
-        void calculate(){
-            mean = sum1/(numOfMeasurements);
-            rms = sqrt(sum2/(numOfMeasurements));
+        int calculate(){
+            
+            if (!sum1) return 1;
+
+            mean = sum1/(numMeasured);
+            rms = sqrt(sum2/(numMeasured-1));
             maxRough = maxy-miny;
             
-            cout<<endl<<endl<<numOfMeasurements<<endl<<endl;
+            ////cout<<endl<<endl<<numOfMeasurements<<endl<<endl;
 
             cout<<mean<<endl<<rms<<endl<<maxRough<<endl;
+            return 0;
         }
 
         void checkForErrors(void){
@@ -90,25 +104,14 @@ class MyInputFileType : public ifstream{
         }
 };
 
-class MyOutputFileType : public ofstream{
-
-};
-
 int main(){
-
+    cout << fixed << setprecision(3);   //formats correctly for constraints
+    
     MyInputFileType inFile;
-    MyOutputFileType outFile;
-
-    double a = 0;
-    double b = 0;
-    double c = 0;
 
     inFile.getInput();
     inFile.open(inFile.inputName);
     inFile.checkForErrors();
     inFile.getMeasurements();
-    inFile.calculate();
-
-
-
+    if (inFile.calculate()) cout<<"\n\nNo data in range.\n";
 }
