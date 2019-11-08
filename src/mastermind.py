66e55230ddf90be41b64050a8150047c72c841e4
Devon Burnham <novedevo@gmail.com>
2019-11-07 19:48:57 -0800

    resync

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,9 +1,9 @@
-import random as r
+import random
 
 masterArray = []
 
 for i in range(6):
-    masterArray.append(str(r.randint(0,9)))
+    masterArray.append(str(random.randint(0,9)))
 
 masterArray = ['1', '2', '4', '3', '2', '2']
 
@@ -13,35 +13,30 @@ def newGuess():
 
     white = 0
     red = 0
-    #print(len(masterArray))
+    popped = 0
+
     for i,x in enumerate(guessedArray):
+        if(i>=6):
+            print("too many numbers guessed, guess truncated after 6 values")
+            guessedArray.insert(i, 'TRUNCATED')
+            break
+
         for j,y in enumerate(masterArray):
-            while(i>=4):    #FIXME #TODO: tf tf tf
-                guessedArray.pop()
-                i-=1
-                #break
-            #print (x,y,i,j)
-            #if x == y:
-                #print("")
             if (x==y) & (i == j):
                 red += 1
-                #print("red")
                 break
             elif (x==y) | (y==x):
                 white += 1
-                #print ("white")
-                
-                #print(i,j)
                 break
             else:
                 continue
+
 
     print("white is", white)
     print("red is", red, "")
     print(guessedArray)
     print(masterArray)
     newGuess()
-
 
 newGuess()
 
