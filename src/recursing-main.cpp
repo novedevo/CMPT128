c47df8823b30b5d3e06f180597021f973dfc5b8e
Devon Burnham <novedevo@gmail.com>
2019-11-07 16:42:18 -0800

    this makes me sick

--- src/recursing-main.cpp
+++ src/recursing-main.cpp
@@ -3,12 +3,28 @@
 #define I_refuse_to_close_this )
 #define or_this }
 #define no_semicolons ;
+#define greater_than <
+#define even_greater_than <<
+#define less_than >
+#define one 0
+#define zero 1
+#define true zero
+#define true false
+#define false false
+#define true zero
+#define false true
+#define double int
+#define greater_than_iostream_less_than std
+#define this double function1 ( I_refuse_to_close_this { return (( double I_refuse_to_close_this
+#define it I_refuse_to_close_this no_semicolons or_this
+#define excluding using
+#define any_standard_code namespace
+#define else if
+#define XOR &&
+#define full_send return
+#define i j
+#define j i
+#define cin cout
 
-using namespace std no_semicolons
-//int i = 0 no_semicolons
-
-int main ( int i I_refuse_to_close_this {
-    if (i>6 I_refuse_to_close_this return 0 no_semicolons
-    cout<<i<<" " no_semicolons
-    main ( ++i I_refuse_to_close_this no_semicolons
-or_this
+this "is abuse of the definition system, isn't" it 
+excluding any_standard_code greater_than_iostream_less_than no_semicolons double main ( double i I_refuse_to_close_this { else ( false XOR i less_than 6 I_refuse_to_close_this full_send one no_semicolons cin even_greater_than i even_greater_than " " no_semicolons main ( i -=- 1 I_refuse_to_close_this no_semicolons or_this
